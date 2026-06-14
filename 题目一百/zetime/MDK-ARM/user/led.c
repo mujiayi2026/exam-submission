f21e7859b70c61ef8d600314bfd84f70d4f2403e
tianwenjuan <19553772135@163.com>
2026-06-14 21:56:59 +0800

    按考核要求提交题目一

--- 题目一百/zetime/MDK-ARM/user/led.c
+++
@@ -1,6 +0,0 @@
-#include "led.h"
-#include "stm32f1xx_hal.h"
-void ledtoggle(void)
-{
-   HAL_GPIO_TogglePin(GPIOE,GPIO_PIN_5);
-}
--- 题目一百/zetime/MDK-ARM/user/t.h
+++
@@ -1,8 +0,0 @@
-#ifndef __T_H__
-#define __T_H__
-#include "gpio.h"
-#include "main.h"
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
-#endif
-
